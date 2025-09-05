0ff1f30c9fca03193235cbaf2580adb4aa29a381
cf9874 <cf9874@gmail.com>
2025-09-06 03:39:41 +0900

    header 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,40 @@ class App extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("hello flutters")),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: (Text('Hello world', style: TextStyle(fontSize: 50))),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'It is, HKS',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                          // fontFamily: 'italic',
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
